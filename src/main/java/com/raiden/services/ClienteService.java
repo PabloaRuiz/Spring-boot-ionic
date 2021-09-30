d3c5b0d0996d6402746405a9eeb2b5cb9afe7b37
Pablo <58537127+PabloaRuiz@users.noreply.github.com>
2021-09-29 21:18:11 -0300

    Ajustando e testando a deleção de cliente #10

--- src/main/java/com/raiden/services/ClienteService.java
+++ src/main/java/com/raiden/services/ClienteService.java
@@ -57,7 +57,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e ){
-			throw new DataIntegrrityException("Não é possivel excluir porque há entidades relacionadas");
+			throw new DataIntegrrityException("Não é possivel excluir porque há pedidos relacionadas");
 		}
 		return null;
 	}
