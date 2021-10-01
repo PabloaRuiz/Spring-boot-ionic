40c7ada56c8234547ab6d271eab0bcd506d6da3e
Pablo <58537127+PabloaRuiz@users.noreply.github.com>
2021-09-30 23:01:57 -0300

    Validação customizada: email não repetido na atualização de cliente #13

--- src/main/java/com/raiden/dto/ClienteDTO.java
+++ src/main/java/com/raiden/dto/ClienteDTO.java
@@ -1,12 +1,14 @@
 package com.raiden.dto;
 
 import com.raiden.domain.Cliente;
+import com.raiden.services.validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
