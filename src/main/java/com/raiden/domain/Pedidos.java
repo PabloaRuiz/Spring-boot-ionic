6a1e5b8b0f0c6f1ac70afe11d254abddb642e373
Pablo <58537127+PabloaRuiz@users.noreply.github.com>
2021-10-02 21:40:21 -0300

    Implementação do subtotal e total do pedido #15

--- src/main/java/com/raiden/domain/Pedidos.java
+++ src/main/java/com/raiden/domain/Pedidos.java
@@ -47,6 +47,16 @@ public class Pedidos implements Serializable{
 		EnderecoDeEntrega = enderecoDeEntrega;
 	}
 
+	public double getValorTotal() {
+		double soma = 0.0;
+		for (ItemPedido ip : itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		return soma;
+
+	}
+
+
 	public Integer getId() {
 		return id;
 	}
