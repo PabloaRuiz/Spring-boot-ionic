f14e2de29496751d07cc30fb6404030888065480
Pablo <58537127+PabloaRuiz@users.noreply.github.com>
2021-10-06 17:26:25 -0300

    Configuração e envio do projeto em produção #19

--- src/main/java/com/raiden/config/DevConfig.java
+++ src/main/java/com/raiden/config/DevConfig.java
@@ -2,6 +2,7 @@ package com.raiden.config;
 
 import com.raiden.services.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,9 +15,15 @@ public class DevConfig {
 
     @Autowired
     private DBService dbService;
+    
+    @Value("${spring.jpa.hibernate.dddl-auto}")
+    private String strategy;
 
     @Bean
     public boolean instantiateDatabase() throws ParseException {
+    	if(!"create".equals(strategy)) {
+    		return false;
+    	}
         dbService.instantiateTestDatabase();
         return true;
     }
